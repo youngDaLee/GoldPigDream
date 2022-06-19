e12630e92b7f5953e1dccb3932e5f3f2ecc0b189
YoungDaLee <ldy0956@gmail.com>
2022-06-19 21:07:59 +0900

    MOD: out code 핸들링

--- BE/database/call_sp.py
+++ BE/database/call_sp.py
@@ -7,4 +7,9 @@ def call_sp_lotto_res_insert(sp_args, cursor=None):
     """
     sp = 'CALL lotto_res_insert(%(round)s, %(lotto_no)s, %(bonus_no)s, %(1st_prize)s, %(2nd_prize)s, %(3rd_prize)s, %(4th_prize)s, %(5th_prize)s, @o);'
     cursor.execute(sp, sp_args)
-    return True
+
+    res = cursor.fetchone()
+    if res['o_out_code'] == 0:
+        return True
+    else:
+        return False
