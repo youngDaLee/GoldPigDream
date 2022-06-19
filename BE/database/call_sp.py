1153b271b97d96772dbf552c9fb58c9733884664
youngDaLee <ldy0956@gmail.com>
2022-06-19 16:10:37 +0900

    ADD: 로또 인서트 SP CALL

--- BE/database/call_sp.py
+++ BE/database/call_sp.py
@@ -1,3 +1,10 @@
 from db_conn import db_conn
 
 
+@db_conn
+def call_sp_lotto_res_insert(sp_args, cursor=None):
+    """
+    """
+    sp = 'CALL lotto_res_insert(%(round)s, %(lotto_no)s, %(bonus_no)s, %(1st_prize)s, %(2nd_prize)s, %(3rd_prize)s, %(4th_prize)s, %(5th_prize)s, @o);'
+    cursor.execute(sp, sp_args)
+    return True
