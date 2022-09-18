aba0d4745c8efb882bc0f9b58682b7906d52865b
YoungDaLee <ldy0956@gmail.com>
2022-09-18 19:53:53 +0900

    Fix: lotto -> game

--- BE/main.py
+++ BE/main.py
@@ -5,13 +5,13 @@ app = FastAPI()
 
 
 from routers import login
-# from routers import lotto
+from routers import game
 from routers import mypage
 from utils.response import response
 
 
 app.include_router(login.router)
-# app.include_router(lotto.router)
+app.include_router(game.router)
 app.include_router(mypage.router)
 
 
