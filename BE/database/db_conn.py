459b20d049bd43a2fb3fa2cf423149ab12d11d26
YoungDaLee <ldy0956@gmail.com>
2022-09-18 20:52:07 +0900

    Fix: Fix connection

--- BE/database/db_conn.py
+++ BE/database/db_conn.py
@@ -31,13 +31,13 @@ try:
     LOTTO_DB = PooledDB(
         creator=pymysql,  # mysql
         mincached=DATABASE['mincached'],
-        maxconnections=DATABASE['maxconnections'],  # 10
+        maxconnections=DATABASE['maxconnections'],
         blocking=True,
-        host=LOTTO_DATABASE['host'],  # 127.0.0.1
-        port=LOTTO_DATABASE['port'],  # 3306
-        user=LOTTO_DATABASE['id'],    # npnc
-        password=LOTTO_DATABASE['pw'],# asdf
-        dbname=LOTTO_DATABASE['db'],  # lotto_db
+        host=LOTTO_DATABASE['host'],
+        port=LOTTO_DATABASE['port'],
+        user=LOTTO_DATABASE['id'],
+        password=LOTTO_DATABASE['pw'],
+        dbname=LOTTO_DATABASE['db'],
         connect_timeout=DATABASE['connect_timeout'],  # 10
         ping=DATABASE['ping'],  # 2
     )
